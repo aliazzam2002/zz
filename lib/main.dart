76c161462a4ad3b2117e6037c116529dd1cc8175
bahaa2000 <Bahajaber2000@gmail.com>
2025-08-08 14:42:23 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: avatars(), theme: ThemeData.dark());
+    return MaterialApp(home: Burger(), theme: ThemeData.dark());
   }
 }
 
