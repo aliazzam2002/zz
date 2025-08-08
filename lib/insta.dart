08c9aaf531244761f4a13cfc2c2c71ff70fa72f4
AliBonez <alialadem20@gmail.com>
2025-08-08 17:02:07 +0300

    new

--- lib/insta.dart
+++ lib/insta.dart
@@ -12,7 +12,7 @@ class Instagram extends StatelessWidget {
           icon: Image.asset("assets/png/Camera.png", color: Colors.white),
         ),
         title: Text(
-          "Belguimgram",
+          "Instagram",
           style: TextStyle(
             fontFamily: "cursive",
             fontWeight: FontWeight.w900,
@@ -37,89 +37,25 @@ class Instagram extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            StoryItem(),
-            SizedBox(width: 16),
-            Column(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.topRight,
-                      colors: [
-                        Color.fromRGBO(226, 3, 55, 1),
-                        Color.fromRGBO(198, 1, 136, 1),
-                        Color.fromRGBO(119, 0, 195, 1),
-                      ],
-                    ),
-                  ),
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    margin: EdgeInsets.all(3),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      margin: EdgeInsets.all(3),
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage("assets/png/Women.png"),
-                        radius: 50,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text("Karennne", style: TextStyle(color: Colors.white)),
-              ],
+            StoryItem(
+              title: "Bahaa Story",
+              storyImage: "assets/png/Man.png",
+              colors: [const Color.fromARGB(255, 251, 255, 0), Colors.cyan],
+              shapes: BoxShape.circle,
+              
             ),
-            SizedBox(width: 16),
-            Column(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.topRight,
-                      colors: [
-                        Color.fromARGB(255, 251, 170, 71),
-                        Color.fromARGB(255, 217, 26, 71),
-                        Color.fromARGB(255, 166, 15, 146),
-                      ],
-                    ),
-                  ),
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    margin: EdgeInsets.all(3),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      margin: EdgeInsets.all(3),
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage("assets/png/Man.png"),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8),
-                Text("Zackjohn", style: TextStyle(color: Colors.white)),
-              ],
+            StoryItem(
+              title: "Ahmad Story",
+              storyImage: "assets/png/Man.png",
+              colors: [const Color.fromARGB(255, 234, 25, 196), Colors.lime],
+              shapes: BoxShape.circle,
+            ),
+            StoryItem(
+              title: "Issac Story",
+              storyImage: "assets/png/Man.png",
+              colors: [const Color.fromARGB(248, 4, 223, 235), Colors.green],
+              shapes: BoxShape.circle,
+              widths: 50,
             ),
           ],
         ),
@@ -133,50 +69,61 @@ class Instagram extends StatelessWidget {
 }
 
 class StoryItem extends StatelessWidget {
-  const StoryItem({super.key});
+  const StoryItem({
+    super.key,
+    required this.title,
+    required this.storyImage,
+    required this.colors,
+    required this.shapes,
+    this.widths,
+  });
+  final String title;
+  final String storyImage;
+  final List<Color> colors;
+  final BoxShape shapes;
+  final double? widths;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            shape: BoxShape.circle,
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.topRight,
-              colors: [
-                Color.fromARGB(255, 251, 170, 71),
-                Color.fromARGB(255, 217, 26, 71),
-                Color.fromARGB(255, 166, 15, 146),
-              ],
-            ),
-          ),
-          child: Container(
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.all(3),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6),
+      child: Column(
+        children: [
+          Container(
+            width: widths ?? 100,
+            height: widths ?? 100,
             decoration: BoxDecoration(
               color: Colors.white,
               shape: BoxShape.circle,
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.topRight,
+                colors: colors,
+              ),
             ),
             child: Container(
               width: 100,
               height: 100,
               margin: EdgeInsets.all(3),
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/png/Man.png"),
-                radius: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+              child: Container(
+                width: 100,
+                height: 100,
+                margin: EdgeInsets.all(3),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage(storyImage),
+                  radius: 50,
+                ),
               ),
             ),
           ),
-        ),
-        SizedBox(height: 8),
-        Text("Your Story", style: TextStyle(color: Colors.white)),
-      ],
+          SizedBox(height: 8),
+          Text(title, style: TextStyle(color: Colors.white)),
+        ],
+      ),
     );
   }
 }
